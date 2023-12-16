91a0bf7648ee4d395dd3816c5e5ca7446347c5b0
sriganeshres <sriganeshthota12345@gmail.com>
2023-12-16 13:39:20 +0530

    All files

---
+++ MinimalistWebServer.c
@@ -0,0 +1,32 @@
+#include <sys/socket.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+void  main()
+{
+	int s =socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in addr = {
+		AF_INET,
+		0x901f,
+		0
+	};
+	bind(s, &addr, sizeof(addr));
+
+	listen(s, 40);
+
+	int client_fd = accept(s, 0, 0);
+
+	char buffer[256] = {0};
+	recv(client_fd, buffer, 256, 0);
+
+	char* f = buffer + 5;
+	*strchr(f, ' ') = 0;
+	int opened_fd = open(f, O_RDONLY);
+	sendfile(client_fd, opened_fd, 0, 256);
+	close(client_fd);
+	close(opened_fd);
+	close(s);
+}

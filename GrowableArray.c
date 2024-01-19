6e9c36f39e540d9b02b28d9ef6eb7c8bdeda1ec6
sriganeshres <sriganeshthota12345@gmail.com>
2024-01-19 11:59:05 +0530

    Made it Generic upto integer, charcter, boolean

--- GrowableArray.c
+++ GrowableArray.c
@@ -1,24 +1,76 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+enum typeReq {
+  INT,
+  CHAR,
+  BOOLEAN,
+};
+
 typedef struct {
-  int *array;
+  void *array;
   size_t used;
   size_t size;
+  enum typeReq typ;
 } Array;
 
-void initArray(Array *a, size_t inital) {
-  a->array = malloc(inital * sizeof(int));
+void initArray(Array *a, size_t inital, enum typeReq typ) {
+  a->typ = typ;
+  switch (typ) {
+  case INT: {
+    a->array = (int *)malloc(inital * sizeof(int));
+    break;
+  }
+  case CHAR: {
+    a->array = (char *)malloc(inital * sizeof(char));
+    break;
+  }
+  case BOOLEAN: {
+    a->array = (bool *)malloc(inital * sizeof(bool));
+    break;
+  }
+  default: {
+    printf("Select a proper type\n");
+    return;
+  }
+  }
   a->used = 0;
   a->size = inital;
 }
 
-void insertArray(Array *a, int val) {
+void insertArray(Array *a, void *val) {
   if (a->used == a->size) {
     a->size *= 2;
-    a->array = realloc(a->array, a->size * sizeof(int));
+    switch (a->typ) {
+    case INT:
+      a->array = (int *)realloc(a->array, a->size * sizeof(int));
+      break;
+    case CHAR:
+      a->array = (char *)realloc(a->array, a->size * sizeof(char));
+      break;
+    case BOOLEAN:
+      a->array = (bool *)realloc(a->array, a->size * sizeof(bool));
+      break;
+    default:
+      printf("Select a proper type\n");
+      return;
+    }
   }
-  a->array[a->used++] = val;
+  switch (a->typ) {
+  case INT:
+    ((int *)a->array)[a->used++] = *((int *)val);
+    break;
+  case CHAR:
+    ((char *)a->array)[a->used++] = *((char *)val);
+    break;
+  case BOOLEAN:
+    ((bool *)a->array)[a->used++] = *((bool *)val);
+    break;
+  default:
+    printf("Select a proper type\n");
+    return;
+  }
 }
 
 void freeArray(Array *a) {
@@ -26,14 +78,32 @@ void freeArray(Array *a) {
   a->array = NULL;
   a->size = a->used = 0;
 }
+
+void printValue(Array *a, size_t index) {
+  switch (a->typ) {
+  case INT:
+    printf("%d\n", ((int *)a->array)[index]);
+    break;
+  case CHAR:
+    printf("%c\n", ((char *)a->array)[index]);
+    break;
+  case BOOLEAN:
+    printf("%s\n", ((bool *)a->array)[index] ? "true" : "false");
+    break;
+  default:
+    printf("Select a proper type\n");
+    break;
+  }
+}
+
 /*
 int main() {
   Array a;
-  initArray(&a, 5);                           USAGE
-  for (int i = 0; i < 100; i++) {
-    insertArray(&a, i);
+  initArray(&a, 5, CHAR);
+  for (int i = 0; i < 100; i++) {                  USAGE
+    insertArray(&a, &(char){i + '0'});
   }
-  printf("%d\n", a.array[11]);
+  printValue(&a, 49);
   printf("%zu", a.used);
   freeArray(&a);
 }
